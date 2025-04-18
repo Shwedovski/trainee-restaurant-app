61a948f7d3342ca2f3b03120725342d72ca53a48
Shwedovski <danya.makovey1@gmail.com>
2025-04-18 17:00:17 +0300

    categories v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CategoriesListComponent],
   templateUrl: 'app.component.html',
   styleUrls: ['./app.component.scss']
 })
