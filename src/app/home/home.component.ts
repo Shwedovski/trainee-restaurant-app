1adf58206d0b12fec265594137faadb10a21ac9f
Shwedovski <danya.makovey1@gmail.com>
2025-04-28 18:33:12 +0300

    categories list added to home page and removed from other pages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CategoriesListComponent } from '../components/categories-list/categories-list.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CategoriesListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
